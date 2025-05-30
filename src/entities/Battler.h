f9c22a65e39732e671a535578e1be1b9b00e9c3b
SepSkye <53712490+Seppy-Dev@users.noreply.github.com>
2025-05-30 23:10:35 +0100

    max hp&mp in battler made variable

--- src/entities/Battler.h
+++ src/entities/Battler.h
@@ -11,11 +11,13 @@ using namespace std;
 
 class Battler {
 public:
-    explicit Battler(const string& name, int hp = MAX_HP, int mp = MAX_MP);
+    explicit Battler(const string& name, int maxHp, int maxMp) : name{name}, maxHp{maxHp}, maxMp{maxMp}, hp{maxHp}, mp{maxMp} {};
     [[nodiscard]] const string& getName() const;
+
     int getHp() const;
     int getMp() const;
-
+    int getMaxHp() const;
+    int getMaxMp() const;
 
     bool isAlive() const;
     bool isFullHp() const;
@@ -27,9 +29,9 @@ public:
     void recoverMp(int amount);
 private:
     const string name;
+    int maxHp;
+    int maxMp;
     int hp;
     int mp;
-    static constexpr int MAX_HP = 100;
-    static constexpr int MAX_MP = 100;
 };
 #endif //BATTLER_H
