d1218a81d26392d021e1c4b685f27b6aed7c392d
SepSkye <53712490+Seppy-Dev@users.noreply.github.com>
2025-05-31 15:46:17 +0100

    Battle menu function removed from main

--- src/main.cpp
+++ src/main.cpp
@@ -83,95 +83,32 @@ void EnemyTurn() {
 }
 
 
-//-----------//
-// Menu area //
-//-----------//
-void BattleMenu() {
-    while (battleActive) {
-        cout << "----------" << endl;
-        cout << player.getName() << endl;
-        cout << "HP: " << player.getHp() << endl;
-        cout << "MP: " << player.getMp() << endl;
-        cout << "----------" << endl << endl;
-
-        cout << "----------" << endl;
-        cout << enemy.getName() << endl;
-        cout << "HP: " << enemy.getHp() << endl;
-        cout << "MP: " << enemy.getMp() << endl;
-        cout << "----------" << endl << endl;
-
-        this_thread::sleep_for(chrono::seconds(1));
-
-        cout << "1. Attack" << endl;
-        cout << "2. Magic (Cost: 15MP)" << endl;
-        cout << "3. Heal (Cost: 20MP)" << endl;
-        cout << "4. Recover" << endl << endl;
-        cout << "Your choice: ";
-
-        int action;
-        while (!(cin >> action)) {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Invalid input! Please enter a number: ";
-        }
-        system("cls");
-
-        switch (action) {
-            case 1:
-                melee.use(player, enemy);
-                break;
-
-            case 2:
-                magic.use(player, enemy);
-                break;
-
-            case 3:
-                heal.use(player);
-                break;
-
-            case 4:
-                recover.use(player);
-                break;
-
-            case 69420: // Secret amazing attack
-                cout << "YOU used LIGHT THAT BURNS MY EYES!" << endl;
-                this_thread::sleep_for(chrono::seconds(2));
-                cout << ".";
-                this_thread::sleep_for(chrono::seconds(1));
-                cout << ".";
-                this_thread::sleep_for(chrono::seconds(1));
-                cout << ".";
-                this_thread::sleep_for(chrono::seconds(1));
-                cout << "!";
-                this_thread::sleep_for(chrono::seconds(1));
-                for (int i = 0; i < 100; i++) {
-                    cout << endl;
-                    for (int j = 0; j < 100; j++)
-                        cout << "#####";
-                }
-                exit(-1);
-
-            default:
-                cout << "Invalid Action!" << endl;
-                continue;
-        }
-
-        if (!enemy.isAlive()) {
-            EndBattle(player);
-        }
-        this_thread::sleep_for(chrono::seconds(1));
-
-        if (battleActive) {
-            EnemyTurn();
-        }
-
-        this_thread::sleep_for(chrono::seconds(1));
-
-        if (!player.isAlive()) {
-            EndBattle(enemy);
-        }
-    }
-}
+// //-----------//
+// // Menu area //
+// //-----------//
+// void BattleMenu() {
+//     while (battleActive) {
+//
+//         this_thread::sleep_for(chrono::seconds(1));
+//
+//
+//
+//         if (!enemy.isAlive()) {
+//             EndBattle(player);
+//         }
+//         this_thread::sleep_for(chrono::seconds(1));
+//
+//         if (battleActive) {
+//             EnemyTurn();
+//         }
+//
+//         this_thread::sleep_for(chrono::seconds(1));
+//
+//         if (!player.isAlive()) {
+//             EndBattle(enemy);
+//         }
+//     }
+// }
 
 
 //------------------//
@@ -179,7 +116,6 @@ void BattleMenu() {
 //------------------//
 int main() {
     srand(time(nullptr));
-    BattleMenu();
     system("pause");
     return 0;
 }
