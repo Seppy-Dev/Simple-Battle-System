f386bb922cab86c3a5c56d8e330c34b64b57582c
SepSkye <53712490+Seppy-Dev@users.noreply.github.com>
2025-06-01 23:59:55 +0100

    Update Battler.h

--- src/entities/Battler.h
+++ src/entities/Battler.h
@@ -6,16 +6,25 @@
 #define BATTLER_H
 
 #include <string>
+#include <nlohmann/json.hpp>
 
 class Battler {
 public:
-    explicit Battler(std::string name, int maxHp = 100, int maxMp = 100, int speed = 10);
+    explicit Battler();
+
+    const std::string& setName(std::string name);
+    int setHp(int hp);
+    int setMp(int mp);
+    int setMaxHp(int maxHp);
+    int setMaxMp(int maxMp);
+    int setSpeed(int speed);
 
     [[nodiscard]] const std::string& getName() const {return name;}
     [[nodiscard]] int getHp() const {return hp;}
     [[nodiscard]] int getMp() const {return mp;}
     [[nodiscard]] int getMaxHp() const {return maxHp;}
     [[nodiscard]] int getMaxMp() const {return maxMp;}
+    [[nodiscard]] int getSpeed() const {return speed;}
 
     [[nodiscard]] bool isAlive() const {return (hp > 0);}
     [[nodiscard]] bool isFullHp() const {return (hp == maxHp);}
@@ -26,9 +35,9 @@ public:
     void recoverHp(int amount);
     void recoverMp(int amount);
 private:
-    const std::string name;
-    const int maxHp;
-    const int maxMp;
+    std::string name;
+    int maxHp;
+    int maxMp;
     int hp;
     int mp;
     int speed;
