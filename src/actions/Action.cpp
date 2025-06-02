70fc908a4b85d9e1344db76e47986eb179fdc5bd
SepSkye <53712490+Seppy-Dev@users.noreply.github.com>
2025-06-02 15:31:55 +0100

    Create Action.cpp

---
+++ src/actions/Action.cpp
@@ -0,0 +1,67 @@
+//
+// Created by Skye on 02/06/2025.
+//
+
+#include "Action.h"
+
+Action::Action(const nlohmann::json& data) {
+    type = getType(data["type"]);
+    category = getCategory(data["category"]);
+    name = data["name"];
+
+    mpCost = data.value("mpCost", 0);
+    hpCost = data.value("hpCost", 0);
+    priority = data.value("priority", 0);
+
+    if (type == Type::ATTACK) {
+        loadAttackProperties(data);
+    }
+    else {
+        loadRecoveryProperties(data);
+    }
+
+}
+Action::Type Action::getType(const nlohmann::json& jsonProperty) {
+    if (jsonProperty == "ATTACK") {
+        return Type::ATTACK;
+    }
+    if (jsonProperty == "RECOVERY") {
+        return Type::RECOVERY;
+    }
+    throw std::invalid_argument("Invalid action type: " + jsonProperty.dump());
+}
+Action::Category Action::getCategory(const nlohmann::json& jsonProperty) {
+    if (jsonProperty == "MAGIC") return Category::MAGIC;
+    if (jsonProperty == "ABILITY") return Category::ABILITY;
+    throw std::invalid_argument("Invalid category: " + jsonProperty.dump());
+}
+
+Action::TargetStat Action::getTargetStat(const nlohmann::json& jsonProperty, const TargetStat defaultStat) {
+    if (jsonProperty == "HP") return TargetStat::HP;
+    if (jsonProperty == "MP") return TargetStat::MP;
+    else return defaultStat;
+}
+
+Action::RecoveryType Action::getRecoveryType(const nlohmann::json& jsonProperty, const RecoveryType defaultType) {
+    if (jsonProperty == "HP") return RecoveryType::HP;
+    if (jsonProperty == "MP") return RecoveryType::MP;
+    else return defaultType;
+}
+
+void Action::loadAttackProperties(const nlohmann::json& data) {
+    targetStat = getTargetStat(data["targetStat"], TargetStat::HP);
+    minDamage = data.value("minDamage", 1);
+    maxDamage = data.value("maxDamage", minDamage);
+    accuracy = data.value("accuracy", 100);
+    critRate = data.value("critRate", 10);
+    multiHitMin = data.value("multiHitMin", 1);
+    multiHitMax = data.value("multiHitMax", 1);
+    hpDrainPercent = data.value("hpDrainPercent", 0);
+    mpDrainPercent = data.value("mpDrainPercent", 0);
+}
+
+void Action::loadRecoveryProperties(const nlohmann::json& data) {
+    minRecovery = data.value("minRecovery", 1);
+    maxRecovery = data.value("maxRecovery", minRecovery);
+    recoveryType = getRecoveryType(data["recoveryType"], RecoveryType::HP);
+}
