e06eb39d182c3addfc08d57b0de49f40a5c3b339
SepSkye <53712490+Seppy-Dev@users.noreply.github.com>
2025-06-02 19:13:45 +0100

    Update EnemyAI.cpp

--- src/battle/EnemyAI.cpp
+++ src/battle/EnemyAI.cpp
@@ -4,45 +4,8 @@
 
 #include "EnemyAI.h"
 
-void EnemyTurn() {
-    int enemyActions[] = {1,2,3,4};
+Action& EnemyTurn(Battler& battler) {
+    Action& action;
 
-    if (enemy.getMp() < darkMagic.getMpCost()) {
-        enemyActions[1] = 0;
-    }
-    if (enemy.getMp() < heal.getMpCost()) {
-        enemyActions[2] = 0;
-    }
-    if (enemy.getHp() > 70) {
-        enemyActions[2] = 0;
-    }
-    if (enemy.getMp() > 20) {
-        enemyActions[3] = 0;
-    }
-
-    int enemyAction = 0;
-    while (enemyAction == 0) {
-        if (int rng = enemyActions[rand() % size(enemyActions)]; rng != 0)
-        {
-            enemyAction = rng;
-            break;
-        }
-    }
-
-    switch (enemyAction) {
-        case 1: darkMelee.use(enemy, player);
-            break;
-
-        case 2: darkMagic.use(enemy, player);
-            break;
-
-        case 3: heal.use(enemy);
-            break;
-
-        case 4: recover.use(enemy);
-            break;
-
-        default:
-            cout << "Enemy action failed! (" << enemyAction << ")" << endl << endl;
-    }
+    return action;
 }
