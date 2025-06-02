4419367d8bd2e0bc9bf44f59d449d09290fe9d2e
SepSkye <53712490+Seppy-Dev@users.noreply.github.com>
2025-06-02 17:44:55 +0100

    Update BattleMenu.cpp

--- src/battle/BattleMenu.cpp
+++ src/battle/BattleMenu.cpp
@@ -2,42 +2,21 @@
 // Created by Skye on 31/05/2025.
 //
 
-#include "BattleMenu.h"
 #include <iostream>
-#include <limits>
-#include <thread>
+#include "BattleMenu.h"
+#include "BattleManager.h"
 
-void BattleMenu::printStats(const Battler& battler) {
-}
 
-void BattleMenu::listActions() {
+void BattleMenu::printStats() {
+    std::cout << "----------" << "\n"
+              << BattleManager::player.getName() << "\n"
+              << "HP: " << BattleManager::player.getHp() << "\n"
+              << "MP: " << BattleManager::player.getMp() << "\n"
+              << "----------\n" <<  std::endl;
 
-}
-
-void selectAction() {
-    while (true) {
-    int action;
-    while (!(std::cin >> action)) {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Invalid input! Please enter a number: ";
-    }
-
-        // case 69420: // Secret amazing attack
-        //      std::cout << "YOU used LIGHT THAT BURNS MY EYES!" << std::endl;
-        //      std::this_thread::sleep_for(std::chrono::seconds(2));
-        //      std::cout << ".";
-        //      std::this_thread::sleep_for(std::chrono::seconds(1));
-        //      std::cout << ".";
-        //      std::this_thread::sleep_for(std::chrono::seconds(1));
-        //      std::cout << ".";
-        //      std::this_thread::sleep_for(std::chrono::seconds(1));
-        //      std::cout << "!";
-        //      std::this_thread::sleep_for(std::chrono::seconds(1));
-        //      for (int i = 0; i < 100; i++) {
-        //         std::cout << std::endl;
-        //         for (int j = 0; j < 100; j++)
-        //             std::cout << "#####";
-        //      exit(-1);
-    }
+    std::cout << "----------" << "\n"
+              << BattleManager::enemy.getName() << "\n"
+              << "HP: " << BattleManager::enemy.getHp() << "\n"
+              << "MP: " << BattleManager::enemy.getMp() << "\n"
+              << "----------\n" <<  std::endl;
 }
