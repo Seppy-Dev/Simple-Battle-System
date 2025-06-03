f146ce04a162e8dcfb28d43ed07477dd533b113d
SepSkye <53712490+Seppy-Dev@users.noreply.github.com>
2025-06-03 21:19:40 +0100

    Add main battle menu

--- src/battle/BattleMenu.cpp
+++ src/battle/BattleMenu.cpp
@@ -22,3 +22,37 @@ void BattleMenu::printStats() {
               << "MP: " << enemy.getMp() << "\n"
               << "----------\n" <<  std::endl;
 }
+
+enum class MENU_OPTIONS {
+    ATTACK = 1,
+    MAGIC = 2,
+    ABILITIES = 3
+};
+
+void BattleMenu::mainBattleMenu() {
+    std::cout << "1. Basic Attack\n"
+              << "2. Magic\n"
+              << "3. Abilities\n" << std::endl;
+    while (true) {
+        std::cout << "Your choice: ";
+        int choice;
+        std::cin >> choice;
+
+        switch (auto option = static_cast<MENU_OPTIONS>(choice)) {
+            case MENU_OPTIONS::ATTACK:
+                std::cout << "Currently Unimplemented!" << std::endl;
+                break;
+            case MENU_OPTIONS::MAGIC:
+                magicMenu();
+                break;
+            case MENU_OPTIONS::ABILITIES:
+                abilityMenu();
+                break;
+            default:
+                std::cout << "Invalid Option!" << std::endl;
+                std::cin.clear();
+                std::cin.ignore();
+        }
+    }
+}
+
