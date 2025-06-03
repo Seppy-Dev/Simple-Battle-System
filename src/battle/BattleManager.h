32394491f9e5e2f02543a1434558b1742a0770a5
SepSkye <53712490+Seppy-Dev@users.noreply.github.com>
2025-06-03 21:20:23 +0100

    Update BattleManager.h

--- src/battle/BattleManager.h
+++ src/battle/BattleManager.h
@@ -7,29 +7,38 @@
 
 #include "../entities/Battler.h"
 #include "../actions/Action.h"
+#include "EnemyAI.h"
 
 class BattleManager {
 public:
-    static Battler player;
-    static Battler enemy;
 
     static void assignPlayer();
     static void assignEnemy();
 
     static void startBattle();
 
-    static Action playerAction();
-
-    static Action enemyAction();
     static void executeTurn();
     static void endBattle(const Battler& winner);
 
     [[nodiscard]] static bool isActive() {return battleActive;}
 
+    static void setPlayerAction(Action& action);
+    static void setEnemyAction(Action& action);
+
+    static Battler& getPlayer() {return player;}
+    static Battler& getEnemy() {return enemy;}
+    static Action& getPlayerAction() {return playerAction;}
+    static Action& getEnemyAction() {return enemyAction;}
+
 private:
-    std::string battlerPath = "../../data/battlers/";
+    std::string battlerPath = "../data/battlers/";
     static bool battleActive;
     static int turns;
+    static Battler player;
+    static Battler enemy;
+    static Action& playerAction;
+    static Action& enemyAction;
+    static EnemyAI& enemyAi;
 };
 
 #endif //BATTLEMANAGER_H
